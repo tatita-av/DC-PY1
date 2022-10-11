c46f1b8fc918a70ac717969c78e585d0166899b2
Татьяна <avramenko.ta@edu.spbstu.ru>
2022-10-11 22:50:12 +0300

    Авраменко Т.А.

---
+++ task_9.py
@@ -0,0 +1,17 @@
+salary = 5000  # зарплата
+spend = 6000  # траты
+months = 10  # количество месяцев
+increase = 0.03  # рост цен
+
+need_money = 0  # количество денег, чтобы прожить 10 месяцев
+
+
+# TODO Оформить решение
+def cringe(need_money, spend, salary, increase):
+    for i in range(10):
+        need_money += spend * ((1 + increase) ** i)
+    need_money -= 10 * salary
+    return need_money
+
+
+print(round(cringe(need_money, spend, salary, increase)))
